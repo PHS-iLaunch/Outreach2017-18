7322571ffb03f30412d3b187be9904cb2a78850b
Robert Frank Zhang <robert@MacBook-Pro.local>
2018-01-02 12:26:53 -0500

    Basic Segues through join/create screens

--- OutreachTest/CreateGroupController.swift
+++ OutreachTest/CreateGroupController.swift
@@ -11,6 +11,31 @@ import UIKit
 
 class CreateGroupController:UIViewController{
     override func viewDidLoad() {
-        view.backgroundColor = .green
+        super.viewDidLoad()
+        setupNavigationBarItems()
+        view.backgroundColor = ThemeColor.whitish
+    }
+    
+    func setupNavigationBarItems(){
+        let createGroupLabel = UILabel()
+        createGroupLabel.text = "Create a New Group"
+        createGroupLabel.textColor = ThemeColor.whitish
+        createGroupLabel.font = UIFont.boldSystemFont(ofSize: 25)
+        navigationItem.titleView = createGroupLabel
+        
+        let backButton = UIButton(type: .system)
+        backButton.setImage(#imageLiteral(resourceName: "x").withRenderingMode(.alwaysOriginal), for: .normal)
+        backButton.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        backButton.addTarget(self, action: #selector(goBack), for: .touchUpInside)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView:backButton)
+        
+        navigationController?.navigationBar.barTintColor = ThemeColor.red
+        let bounds = self.navigationController!.navigationBar.bounds
+        navigationController?.navigationBar.frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height*1.5)
+        navigationController?.navigationBar.isTranslucent = false
+    }
+    
+    func goBack(){
+        self.dismiss(animated: true, completion: nil)
     }
 }
--- OutreachTest/JoinGroupController.swift
+++ OutreachTest/JoinGroupController.swift
@@ -11,6 +11,31 @@ import UIKit
 
 class JoinGroupController:UIViewController{
     override func viewDidLoad() {
-        view.backgroundColor = .red
+        super.viewDidLoad()
+        setupNavigationBarItems()
+        view.backgroundColor = ThemeColor.whitish
+    }
+    
+    func setupNavigationBarItems(){
+        let joinGroupLabel = UILabel()
+        joinGroupLabel.text = "Join a Group"
+        joinGroupLabel.textColor = ThemeColor.whitish
+        joinGroupLabel.font = UIFont.boldSystemFont(ofSize: 25)
+        navigationItem.titleView = joinGroupLabel
+        
+        let backButton = UIButton(type: .system)
+        backButton.setImage(#imageLiteral(resourceName: "x").withRenderingMode(.alwaysOriginal), for: .normal)
+        backButton.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        backButton.addTarget(self, action: #selector(goBack), for: .touchUpInside)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView:backButton)
+        
+        navigationController?.navigationBar.barTintColor = ThemeColor.red
+        let bounds = self.navigationController!.navigationBar.bounds
+        navigationController?.navigationBar.frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height*1.5)
+        navigationController?.navigationBar.isTranslucent = false
+    }
+    
+    func goBack(){
+        self.dismiss(animated: true, completion: nil)
     }
 }
